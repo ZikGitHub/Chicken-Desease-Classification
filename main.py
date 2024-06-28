f832d9d5e9e52196533c0b70f913239c305ddd9d
Zishan <zishanenator@gmail.com>
2024-06-28 23:55:44 +0530

    all stages clear

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from cnnClassifier import logger
 from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from cnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+from cnnClassifier.pipeline.stage_03_training import PrepareCallback, Training, ConfigurationManager
+from cnnClassifier.pipeline.stage_04_evaluation import ConfigurationManager, Evaluation
+
+
 
 STAGE_NAME = "Data Ingestion stage"
-
 try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
    data_ingestion = DataIngestionTrainingPipeline()
@@ -12,6 +15,7 @@ try:
 except Exception as e:
         logger.exception(e)
         raise e
+
 
 STAGE_NAME = "Prepare base model"
 try: 
@@ -23,4 +27,37 @@ try:
 except Exception as e:
         logger.exception(e)
         raise e
+
+
+STAGE_NAME = 'Training'
+try:
+    config = ConfigurationManager()
+    prepare_callbacks_config = config.get_prepare_callback_config()
+    prepare_callbacks = PrepareCallback(config=prepare_callbacks_config)
+    callback_list = prepare_callbacks.get_tb_ckpt_callbacks()
+
+    training_config = config.get_training_config()
+    training = Training(config=training_config)
+    training.get_base_model()
+    training.train_valid_generator()
+    training.train(
+        callback_list=callback_list
+    )
+    
+except Exception as e:
+    raise e
+
+
+
+STAGE_NAME = 'Evaluation stage'
+try:
+    config = ConfigurationManager()
+    val_config = config.get_validation_config()
+    evaluation = Evaluation(val_config)
+    evaluation.evaluation()
+    evaluation.save_score()
+
+except Exception as e:
+   raise e
+
 
